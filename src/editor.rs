f5f563910457e9ec1eeaaee25f666d4554144fdc
Bthxtly <bthxtly@gmail.com>
2025-10-22 14:42:28 +0800

    feat: use buffer to reduce flicker effect

--- src/editor.rs
+++ src/editor.rs
@@ -5,7 +5,7 @@ use crossterm::event::{
 };
 
 mod terminal;
-use terminal::Terminal;
+use terminal::{Position, Size, Terminal};
 
 pub struct Editor {
     should_quit: bool,
@@ -52,22 +52,27 @@ impl Editor {
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        Terminal::hide_cursor()?;
         if self.should_quit {
             Terminal::clear_screen()?;
-            println!("Goodbye.");
+            Terminal::print("Goodbye.\r\n")?;
         } else {
             Self::draw_rows()?;
-            Terminal::move_cursor_to(0, 0)?;
+            Terminal::move_cursor_to(Position { x: 0, y: 0 })?;
         }
+        Terminal::show_cursor()?;
+        Terminal::execute()?;
         Ok(())
     }
 
     fn draw_rows() -> Result<(), std::io::Error> {
-        let height = Terminal::size()?.1;
+        let Size { height, .. } = Terminal::size()?;
         for _ in 1..height {
-            print!("~\r\n");
+            Terminal::clear_line()?;
+            Terminal::print("~\r\n")?;
         }
-        print!("~");
+        Terminal::clear_line()?;
+        Terminal::print("~")?;
         Ok(())
     }
 }
--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -7,6 +7,7 @@ pub struct Terminal {}
 
 impl Terminal {
     pub fn terminate() -> Result<(), std::io::Error> {
+        Self::execute()?;
         disable_raw_mode()?;
         Ok(())
     }
@@ -15,20 +16,29 @@ impl Terminal {
         enable_raw_mode()?;
         Self::clear_screen()?;
         Self::move_cursor_to(0, 0)?;
+        Self::execute()?;
         Ok(())
     }
 
     pub fn clear_screen() -> Result<(), std::io::Error> {
-        execute!(stdout(), Clear(ClearType::All))?;
+        queue!(stdout(), Clear(ClearType::All))?;
         Ok(())
     }
 
     pub fn move_cursor_to(x: u16, y: u16) -> Result<(), std::io::Error> {
-        execute!(stdout(), MoveTo(x, y))?;
+        queue!(stdout(), MoveTo(p.x, p.y))?;
         Ok(())
     }
 
     pub fn size() -> Result<(u16, u16), std::io::Error> {
         size()
+    pub fn print(s: &str) -> Result<(), std::io::Error> {
+        queue!(stdout(), Print(s))?;
+        Ok(())
+    }
+
+    pub fn execute() -> Result<(), std::io::Error> {
+        stdout().flush()?;
+        Ok(())
     }
 }
