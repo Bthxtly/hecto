8cd7cfb860767afb209d6858b2150a0fd1d6d645
Bthxtly <bthxtly@gmail.com>
2025-10-22 16:11:58 +0800

    feat: add welcome message at start up

--- src/editor.rs
+++ src/editor.rs
@@ -7,6 +7,9 @@ use crossterm::event::{
 mod terminal;
 use terminal::{Position, Size, Terminal};
 
+const NAME: &str = env!("CARGO_PKG_NAME");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
 pub struct Editor {
     should_quit: bool,
 }
@@ -65,14 +68,44 @@ impl Editor {
         Ok(())
     }
 
+    fn draw_welcome_message() -> Result<(), std::io::Error> {
+        let mut welcome_message = format!("{NAME} editor -- version {VERSION}");
+
+        let width = Terminal::size()?.width as usize;
+        let len = welcome_message.len();
+        if width < len {
+            welcome_message = "~".to_string(); // don't display welcome message if too narrow
+        } else {
+            let padding = (width - len) / 2;
+
+            let spaces = " ".repeat(padding - 1);
+            welcome_message = format!("~{spaces}{welcome_message}");
+            welcome_message.truncate(width);
+        }
+
+        Terminal::print(welcome_message.as_ref())?;
+        Ok(())
+    }
+
+    fn draw_empty_row() -> Result<(), std::io::Error> {
+        Terminal::print("~")?;
+        Ok(())
+    }
+
     fn draw_rows() -> Result<(), std::io::Error> {
         let Size { height, .. } = Terminal::size()?;
-        for _ in 1..height {
+        for current_row in 0..height {
             Terminal::clear_line()?;
-            Terminal::print("~\r\n")?;
+            if current_row == height / 3 {
+                Self::draw_welcome_message()?;
+            } else {
+                Self::draw_empty_row()?;
+            }
+
+            if current_row + 1 < height {
+                Terminal::print("\r\n")?;
+            }
         }
-        Terminal::clear_line()?;
-        Terminal::print("~")?;
         Ok(())
     }
 }
