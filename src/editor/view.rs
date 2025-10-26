dbc6e5dd10a87ab889b7d4e66dc226af3c5bb480
Bthxtly <bthxtly@gmail.com>
2025-10-26 18:49:36 +0800

    refactor: render file content and welcome message separately

--- src/editor/view.rs
+++ src/editor/view.rs
@@ -37,21 +37,16 @@ impl View {
         Ok(())
     }
 
-    pub fn render(&self) -> Result<(), std::io::Error> {
+    fn render_welcome_screen() -> Result<(), std::io::Error> {
         let Size { height, .. } = Terminal::size()?;
+
         for current_row in 0..height {
             Terminal::clear_line()?;
-
-            if let Some(s) = self.buffer.lines.get(current_row) {
-                Terminal::print(s)?;
-                Terminal::print("\r\n")?;
-                continue;
-            }
 
             // we allow this since we don't care if our welcome message is put _exactly_ in the middle.
             // it's allowed to be a bit up or down
             #[allow(clippy::integer_division)]
-            if self.buffer.is_empty() && current_row == height / 3 {
+            if current_row == height / 3 {
                 Self::draw_welcome_message()?;
             } else {
                 Self::draw_empty_row()?;
@@ -62,6 +57,36 @@ impl View {
             if current_row + 1 < height {
                 Terminal::print("\r\n")?;
             }
+        }
+        Ok(())
+    }
+
+    fn render_buffer(&self) -> Result<(), std::io::Error> {
+        let Size { height, .. } = Terminal::size()?;
+
+        for current_row in 0..height {
+            Terminal::clear_line()?;
+
+            if let Some(s) = self.buffer.lines.get(current_row) {
+                Terminal::print(s)?;
+            } else {
+                Self::draw_empty_row()?;
+            }
+
+            // `current_row + 1` should not overflow, unless `height` is usize::MAX XD
+            #[allow(clippy::arithmetic_side_effects)]
+            if current_row + 1 < height {
+                Terminal::print("\r\n")?;
+            }
+        }
+        Ok(())
+    }
+
+    pub fn render(&self) -> Result<(), std::io::Error> {
+        if self.buffer.is_empty() {
+            Self::render_welcome_screen()?;
+        } else {
+            self.render_buffer()?;
         }
         Ok(())
     }
